875d92862b50944ba1fecb9f6bc45e9a74b002a7
noahbachmann <dev@noahbachmann.ch>
2026-08-03 10:53:08 +0200

    solver: add second row iteration

--- shared/src/commonMain/kotlin/com/trainpaths/nonogram/classes/Solver.kt
+++ shared/src/commonMain/kotlin/com/trainpaths/nonogram/classes/Solver.kt
@@ -134,25 +134,91 @@ class Solver(val ng: Nonogram) {
         }
         trackingCols.clear()
 
+        for (row in trackingRows) {
+            val clues: List<Int> = ng.rowClues[row]
+
+            //left check for constraints
+            var left = clues[0]
+            var leftCount = 0
+            while (solution[row][left + leftCount] == 1) {
+                trackingCols.add(leftCount)
+                solution[row][leftCount++] = 2
+            }
+            left += leftCount
+            for (col in leftCount until left) {
+                if (solution[row][col] == 1) {
+                    var len = 0
+                    for (c in col until left) {
+                        solution[row][c] = 1
+                        len++
+                    }
+                    if (len == clues[0]) {
+                        solution[row][left] = 2
+                        trackingCols.addAll(row until left + 1)
+                    } else {
+                        trackingCols.addAll(row until left)
+                    }
+                    break
+                }
+            }
+
+            //right check for constraints
+            var right = ng.width - clues.last()
+            var rightCount = 1
+
+            while (solution[row][right - rightCount] == 1) {
+                val newRight = ng.width - rightCount
+
+                trackingCols.add(newRight)
+                solution[row][newRight] = 2
+
+                rightCount++
+            }
+            right -= --rightCount
+
+            val rightEnd = ng.width - rightCount
+            for (col in right until rightEnd) {
+                if (solution[row][col] == 1) {
+                    var len = 1
+                    for (c in right until col) {
+                        solution[row][c] = 1
+                        len++
+                    }
+                    if (len == clues.last()) {
+                        solution[row][right--] = 2
+                        trackingCols.addAll(right until ng.width)
+                    } else {
+                        trackingCols.addAll(right until col)
+                    }
+                    break
+                }
+            }
+        }
+        trackingRows.clear()
+
         return solution
     }
 
-    private fun drawRowTiles(l: Int, r: Int, row: Int, index: Int) {
+    private fun drawRowTiles(l: Int, r: Int, row: Int, index: Int?) {
         for (col in l until r) {
             val cell = solving[row][col]
             cell.state = 1
-            cell.posRowClues.add(index)
+            if (index != null) {
+                cell.posRowClues.add(index)
+            }
 
             solution[row][col] = 1
             trackingCols.add(col)
         }
     }
 
-    private fun drawColTiles(l: Int, r: Int, col: Int, index: Int) {
+    private fun drawColTiles(l: Int, r: Int, col: Int, index: Int?) {
         for (row in l until r) {
             val cell = solving[row][col]
             cell.state = 1
-            cell.posColClues.add(index)
+            if (index != null) {
+                cell.posColClues.add(index)
+            }
 
             solution[row][col] = 1
             trackingRows.add(row)
