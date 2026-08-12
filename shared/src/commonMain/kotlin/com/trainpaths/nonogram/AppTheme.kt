0f811681d163436886abbc1639e0db7b1f13b9db
noahbachmann <dev@noahbachmann.ch>
2026-08-12 16:12:40 +0200

    feat: change ColorTheme to use new orange color

--- shared/src/commonMain/kotlin/com/trainpaths/nonogram/AppTheme.kt
+++ shared/src/commonMain/kotlin/com/trainpaths/nonogram/AppTheme.kt
@@ -8,9 +8,10 @@ import androidx.compose.ui.graphics.Color
 
 private val Colors: ColorScheme = lightColorScheme(
     primary = Color(0xFF153D36),
-    onPrimary = Color.White,
-    secondary = Color(0xFFC2EFFF),
+    onPrimary = Color(0xFFEF7F71),
+    secondary = Color(0xFFEF7F71),
     tertiary = Color(0xFFFFD700),
+    outline = Color(0xFF9A9A9A),
     background = Color(0xFF153D36),
     onBackground = Color.White,
 )
