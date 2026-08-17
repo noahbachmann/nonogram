0176e55d7301f12345cd3190022c34df65ca3f85
noahbachmann <dev@noahbachmann.ch>
2026-08-17 10:44:53 +0200

    feat: new secondary Color + change toolbar coloring

--- shared/src/commonMain/kotlin/com/trainpaths/nonogram/AppTheme.kt
+++ shared/src/commonMain/kotlin/com/trainpaths/nonogram/AppTheme.kt
@@ -9,7 +9,8 @@ import androidx.compose.ui.graphics.Color
 private val Colors: ColorScheme = lightColorScheme(
     primary = Color(0xFF153D36),
     onPrimary = Color(0xFFEF7F71),
-    secondary = Color(0xFFEF7F71),
+    secondary = Color(0xFF0E2620),
+    onSecondary = Color(0xFFE55D4C),
     tertiary = Color(0xFFFFD700),
     outline = Color(0xFF9A9A9A),
     background = Color(0xFF153D36),
